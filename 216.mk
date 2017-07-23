cf621c0d52c449e3410aac67ad8c6b49c8467f4b
FlyingJester <foolkingcrown@gmail.com>
2017-07-23 02:12:11 +0100

    Updated makefile

--- 216.mk
+++ 216.mk
@@ -5,7 +5,7 @@ CC=$(PREFIX)gcc
 CXX=$(PREFIX)g++
 GOLD=$(PREFIX)ld.gold
 OBJCOPY=$(PREFIX)objcopy
-CCFLAGS=-Wall -Wextra -Werror -g -fno-builtin -nostdlib -ffreestanding -pipe -I$(ROOT)/libc/include
+CCFLAGS=-Wall -Wextra -Werror -g -fno-builtin -nostdlib -ffreestanding -pipe -I$(ROOT)/libc/include -I$(ROOT)/libcpp/include
 CFLAGS=$(CCFLAGS) -Os -ansi -Wenum-compare -Wjump-misses-init -Wmissing-prototypes
 CXXFLAGS=$(CCFLAGS) -fno-exceptions -fno-rtti -std=c++03
 AR=$(PREFIX)ar
